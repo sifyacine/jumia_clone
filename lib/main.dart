f6b268ba062196e373fa359a703b2aead41dbba7
YCN <89133149+sifyacine@users.noreply.github.com>
2024-08-29 21:12:15 +0100

    setting up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
   // Todo: Add Widgets Binding
   // Todo: Init Local Storage
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
-  // Todo: Initialize Authentication
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform).then((
+          FirebaseApp value) => Get.put(AuthenticationRepository()));
 
+      // Todo: Initialize Authentication
 
-  runApp(MyApp());
+      runApp(MyApp());
 }
-
